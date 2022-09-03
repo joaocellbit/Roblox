408600ae65f0ad4399add508286ad45425e8a3b3
joaocellbit <85044187+joaocellbit@users.noreply.github.com>
2022-09-02 22:15:48 -0300

    Create geneticroll.lua

---
+++ geneticroll.lua
@@ -0,0 +1,35 @@
+local genetica = {
+	
+	Saiyan = { ['Low class'] = 74, ['Elite'] = 20, ['Kaioh soul'] = 5, ['Legendary'] = 1
+		
+	},
+	FrostDemon = {['Mutant'] = 1, ['Cooler'] = 5,["King Cold"] = 20, ['Frost'] = 74 
+		
+	},
+	Earthling = {['Human'] = 74, ['Half Saiyan'] = 25, ['Ancient'] = 1	
+		
+	},
+	Namek = {['Dragon Clan'] = 74, ['Warrior Clan'] = 20, ['Ancient'] = 5, ['Demon clan'] = 1
+		
+	},
+	Jinago = {['Sealed Power'] = 74, ['Dormant Power']= 20, ['Awakened Power'] = 5, ['Jiren'] = 1
+		
+	},
+	Sphinix = {
+		['Dormant Power'] = 74, ['Pride Troopers'] = 20, ['God Potential'] = 5, ['Awakened God'] = 1
+		
+	},
+	Alien = {
+		['Freeza Soldier'] = 74, ['Yadrat'] = 20, ['Bojack'] = 5, ['Hit'] = 1
+	},
+	Majin = {
+		['Clone Buu'] = 74, ['Good Buu'] = 20, ['Kaioh Buu'] = 5, ['True Buu'] = 1
+	},
+	Demon = {
+		['Pure Blood'] = 74, ['Demon Warrior'] = 20, ['Hybrid'] = 5, ['Demigra'] = 1
+	}
+}
+
+
+
+return genetica
