80b5e93871ef5e0c3894f7961ed97488af49c662
joaocellbit <85044187+joaocellbit@users.noreply.github.com>
2022-09-02 22:16:28 -0300

    Create racaroll.lua

---
+++ racaroll.lua
@@ -0,0 +1,25 @@
+local raceroll = { 
+	racas = {
+		Saiyan = 10, 
+		
+		['Frost Demon'] = 12, 
+		
+		Namek = 20, 
+		
+		Jinago = 3, 
+		
+		Sphinix = 1, 
+		
+		Earthling = 25, 
+		
+		Majin = 3, 
+		
+		Alien = 25,	
+		
+		Demon = 1
+		
+}
+	
+}
+
+return raceroll
